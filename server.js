1e05b748e708471af6595c572d186e7d2cc23510
Raquel Gonzalez <raquelgonzalez@Raquels-MacBook-Pro.local>
2026-02-09 23:47:52 -0800

    done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./data/mockDatabase");
+const db = require("./mockDatabase");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
